ee23f964ec2903e3148a1ed7482056b78ca46dac
yasinakarsu <yasin.akarsu@inomera.com>
2022-04-08 12:15:18 +0300

    add header component & layout and styles

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { LoginRegisterModule } from 'src/app/custom/login-register/login-register.module'
+import { LoginRegisterModule } from 'src/app/custom/login-register/login-register.module';
+import { NavigationBarComponent } from './custom/navigation-bar/navigation-bar.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavigationBarComponent
   ],
   imports: [
     BrowserModule,
