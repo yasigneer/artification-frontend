8cb0b0dee267761a10b84ba4025fab8d6664a042
yasinakarsu <yasin.akarsu@inomera.com>
2022-04-25 16:32:01 +0300

    set route configs not finished

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import {LoginRegisterComponent} from "./custom/login-register/login-register/login-register.component";
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'artification-frontend';
+  showHeader = true;
+  determineComponent(event: any){
+    this.showHeader = !(event instanceof LoginRegisterComponent);
+  }
 }
