6452b5b2421cd4a0e78d08fdff34b22d90216bb6
yasinakarsu <yasin.akarsu@inomera.com>
2022-04-27 02:46:11 +0300

    add register service and form group

---
+++ src/app/services/register.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+
+import {User} from "../models/user.model";
+import {environment} from "../../environments/environment";
+import {first} from "rxjs/operators";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class RegisterService {
+  private apiUrl=  `${environment.apiUrl}api/Register`
+  constructor(protected httpClient: HttpClient) { }
+  register(user: User){
+    return this.httpClient.post(this.apiUrl,user).pipe(first())
+  }
+}
